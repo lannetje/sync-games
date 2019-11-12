51f1c41a5397079773d0545f7126d0170fd51ab0
lannetje <yourivdlans+lannetje@gmail.com>
2019-11-12 20:38:10 +0100

    Added dialog for server ip

--- sync-games.ps1
+++ sync-games.ps1
@@ -1,24 +1,31 @@
 Add-Type -AssemblyName System.Windows.Forms
+[void][System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic')
 
-$settingsFile = 'sync-games-path.txt'
+$pathFile = 'sync-games-path.txt'
+$ipFile = 'sync-games-host.txt'
 
-$path = Get-Content $settingsFile
- 
+$path = Get-Content $pathFile
+$serverIp = Get-Content $ipFile
+
+$newServerIp = [Microsoft.VisualBasic.Interaction]::InputBox("Enter server IP", "IP", $serverIp)
+
+$newServerIp > $ipFile
+
 $FolderBrowser = New-Object System.Windows.Forms.FolderBrowserDialog -Property @{
     Description = 'Pick the path you want the games synced to'
     RootFolder = 0x11
     ShowNewFolderButton = $True
     SelectedPath = $path
 }
- 
+
 $result = $FolderBrowser.ShowDialog()
 
 if ($result -eq [Windows.Forms.DialogResult]::Ok) {
     $newpath = $FolderBrowser.SelectedPath
 
-    $newpath > $settingsFile
+    $newpath > $pathFile
 
-    .\winscp.com /command "open ftp://lannetje:lan@192.168.178.49" "synchronize local -criteria=none """"$newpath"""" /" "exit"
+    .\winscp.com /command "open ftp://lannetje:lan@$newServerIp" "synchronize local -criteria=none """"$newpath"""" /" "exit"
 } else {
     'Aborted...'
 }
